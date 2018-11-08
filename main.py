39466857f4efd64a2ecaf3bb697f3047a571572f
Jim Xia <jxia@scu.edu>
2018-11-07 18:26:14 -0800

    2000 stage added

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from core import Machine
+from dialogflow_v2 import DialogflowApi
+import json
+import time
 
+data_format = {
+                'header':[0, 0, 0, 0, int(time.time()), 3, 0],
+                'data':{
+                        'speech': None,
+                        'entity': None
+                }
+}
 
 class Waze(object):
     pass
@@ -18,10 +28,44 @@ transitions = [
 machine = Machine(model=user, states=states, transitions=transitions, initial='first')
 
 
-# Test
-print(user.state)
-user.navigation()
-print(user.state)
-user.navigation_cancel()
-print(user.state)
-user.navigation_cancel()
+def manager(data):
+    """
+    :type data: String
+    :rtype: String
+    """
+
+    print(data)
+    data_json = json.loads(data)
+
+    try:
+        header = data_json['header']
+        print(header)
+        query = data_json['data']['query']
+        data_format['header'][0] = data_json['header'][0]
+        data_format['header'][1] = data_json['header'][1]
+        data_format['header'][2] = data_json['header'][2]
+        data_format['header'][3] = data_json['header'][2]
+    except KeyError:
+        print('in_data: KeyError')
+
+    a = DialogflowApi()
+    response = a.text_query(query)
+    print(response.json())
+
+    data = response.json()
+
+    try:
+        data_format['data']['speech'] = data['queryResult']['fulfillmentText']
+        data_format['header'][6] = len(data['queryResult']['fulfillmentText'])
+    except KeyError:
+        data_format['data']['speech'] = 'KeyError'
+
+    if data['queryResult']['intent']['displayName'] == 'waze.nav.explicit':
+        data_format['header'][3] = 2000
+
+    try:
+        data_format['data']['entity'] = data['queryResult']['parameters']
+    except KeyError:
+        print('No Entity detected')
+
+    return json.dumps(data_format)
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import asyncio
 import websockets
 import json
 import time
-from dialogflow_v2 import DialogflowApi
+from main import manager
 
 data_format = {
                 'header':[0, 0, 0, 0, int(time.time()), 3, 0],
@@ -20,37 +20,9 @@ async def ws_server(ws, path):
     while True:
         try:
             in_data = await ws.recv()
-            print(in_data)
-            in_json = json.loads(in_data)
-            try:
-                header = in_json['header']
-                print(header)
-                query = in_json['data']['query']
-                data_format['header'][0] = in_json['header'][0]
-                data_format['header'][1] = in_json['header'][1]
-                data_format['header'][2] = in_json['header'][2]
-                data_format['header'][3] = in_json['header'][2]
-            except KeyError:
-                print('in_data: KeyError')
-                break
 
-            a = DialogflowApi()
-            response = a.text_query(query)
-            print(response.json())
-
-            data = response.json()
-            try:
-                data_format['data']['speech'] = data['queryResult']['fulfillmentText']
-                data_format['header'][6] = len(data['queryResult']['fulfillmentText'])
-            except KeyError:
-                data_format['data']['speech'] = 'KeyError'
-
-            try:
-                data_format['data']['entity'] = data['queryResult']['parameters']
-            except KeyError:
-                print('No Entity detected')
-            
-            await ws.send(json.dumps(data_format))
+            out_data = manager(in_data)
+            await ws.send(out_data)
         
         except websockets.exceptions.ConnectionClosed:
             print('disconnected')
