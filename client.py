64912582daef96248c97e65d489044d2229c9434
Jim <jxia@alumni.scu.edu>
2018-12-17 15:34:38 -0800

    add loop

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import websockets
 import json
 
 quest = {
-            'header': [1,2,3,4,5,6],
+            'header': [1111, 1, 1000, 4, 5, 6],
             'data': {
                 'query': None,
                 'entity': None
@@ -18,12 +18,12 @@ async def hello():
     async with websockets.connect(
             'ws://178.128.181.252/ws/') as websocket:
 
-        quest['data']['query'] = input("Query:")
+        while True:
+            quest['data']['query'] = input("Query:")
 
+            await websocket.send(json.dumps(quest))
 
-        await websocket.send(json.dumps(quest))
-
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
+            greeting = await websocket.recv()
+            print(f"< {greeting}")
 
 asyncio.get_event_loop().run_until_complete(hello())
