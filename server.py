b54dca731c5bf5042c82e70c1cbae4df6a356080
Jim Xia <jxia@scu.edu>
2019-01-29 15:58:41 -0800

    log clean

--- server.py
+++ server.py
@@ -17,8 +17,8 @@ asyncoronize
 """
 
 logger = logging.getLogger(__name__)
-logging.getLogger("requests").setLevel(logging.DEBUG)
-logging.getLogger("urllib3").setLevel(logging.DEBUG)
+logging.getLogger("requests").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
 setup_logging(default_path='utils/logging.json')
 
 connected = set()
