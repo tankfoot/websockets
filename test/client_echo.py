9e71df195bf0506d8ca3ed8192da7fb0b0c90429
Jim <jxia@alumni.scu.edu>
2019-02-19 10:27:34 -0800

    add server echo client tester

--- test/client_echo.py
+++ test/client_echo.py
@@ -4,20 +4,23 @@
 
 import asyncio
 import websockets
-import json
+import string
 import time
+import random
 
 
 async def hello():
     async with websockets.connect(
             'ws://142.93.91.246/ws_echo/') as websocket:
 
-        while True:
-            in_data = input("query:")
+        for i in range(6):
+            in_data = ''.join(random.choices(string.ascii_uppercase + string.digits, k=1000000))
 
+            start = time.time()
             await websocket.send(in_data)
-
             greeting = await websocket.recv()
-            print(f"< {greeting}")
+            end = time.time()
+            #print(f"< {greeting}")
+            print("response time: {}".format(end - start))
 
 asyncio.get_event_loop().run_until_complete(hello())
