9d977f3fc63d61a85f0f505fe7149e69cd0d1ecf
Jim Xia <jxia@scu.edu>
2019-03-05 15:12:37 -0800

    debug

---
+++ client_buffer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# WS client example
+
+import asyncio
+import websockets
+import json
+import time
+import base64
+
+quest = {
+            'header': [1111, 0, 100, int(time.time()), 3, 0, 1],
+            'data': {
+                'audio': None
+            }
+}
+
+
+async def hello():
+    async with websockets.connect(
+            'ws://localhost:8765') as websocket:
+
+        start = time.time()
+        with open("data/test.wav", "rb") as speech:
+            content = speech.read()
+
+        n = 512
+        stream = [content[i:i + n] for i in range(0, len(content), n)]
+
+        for chunk in stream:
+            quest['data']['audio'] = base64.b64encode(chunk).decode('utf-8')
+            await websocket.send(json.dumps(quest))
+
+        quest['header'][6] = 0
+        await websocket.send(json.dumps(quest))
+        greeting = await websocket.recv()
+        stop = time.time()
+        print(f"< {greeting}")
+        print('round trip time: {}'.format(round(stop - start, 5)))
+
+asyncio.get_event_loop().run_until_complete(hello())
--- server.py
+++ server.py
@@ -8,6 +8,12 @@ import logging
 import json
 from utils.log_utils import setup_logging
 import time
+import datetime
+import base64
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
 from main import state_init
 from main import manager
 
@@ -25,31 +31,91 @@ setup_logging(default_path='utils/logging.json')
 connected = set()
 
 
+def speech_api(stream):
+
+    client = speech.SpeechClient()
+
+    requests = (types.StreamingRecognizeRequest(audio_content=chunk)
+                for chunk in stream)
+
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='en-US')
+
+    streaming_config = types.StreamingRecognitionConfig(
+        config=config,
+        interim_results=False,
+        single_utterance=False)
+
+    responses = client.streaming_recognize(streaming_config, requests)
+
+    return responses
+
+
+def print_response(r):
+    for response in r:
+        # Once the transcription has settled, the first result will contain the
+        # is_final result. The other results will be for subsequent portions of
+        # the audio.
+        for result in response.results:
+            #print('Finished: {}'.format(result.is_final))
+            #print('Stability: {}'.format(result.stability))
+            alternatives = result.alternatives
+            # The alternatives are ordered from most likely to least.
+            for alternative in alternatives:
+                #print('Confidence: {}'.format(alternative.confidence))
+                #print(u'Transcript: {}'.format(alternative.transcript))
+                return alternative.transcript
+
+
 async def ws_server(ws, path):
 
-    connected.add(ws)
-    print('current: ')
-    print(connected)
+    stream = []
+    try:
+        async for message in ws:
+            d = json.loads(message)
+            if 'audio' in d['data']:
+                stream.append(base64.b64decode(d['data']['audio']))
+            else:
+                pass
+                #print(message)
 
-    while True:
-        try:
-            in_data = await ws.recv()
-            logging.info(in_data)
-            state_init(in_data)
-            out_data = manager(in_data)
-            logger.info(out_data)
-            await ws.send(out_data)
-        
-        except websockets.exceptions.ConnectionClosed:
-            '''
-            TODO: Logging.info
-            '''
-            print('{}: user disconnected'.format(int(time.time())))
-            connected.remove(ws)
-            print(connected)
-            break
+            if d['header'][6] == 0:
+                out = d
+                responses = speech_api(stream)
+
+                start = time.time()
+                res = print_response(responses)
+                if res:
+                    out['data']['query'] = res
+                    del out['header'][6]
+                    del out['data']['audio']
+                    print(out)
+                    state_init(json.dumps(out))
+                    out_data = manager(json.dumps(out))
+                else:
+                    pass
+                stop = time.time()
+
+                with open(f"output/{datetime.datetime.now():%Y-%m-%dT%H%M%S}_{res}.pcm", mode='bx') as f:
+                    for chunk in stream:
+                        f.write(chunk)
+
+                stream = []
+                #out['data']['result'] = res
+
+                print("response time: {}".format(round(stop - start, 5)))
+                await ws.send(out_data)
+
+    except websockets.exceptions.ConnectionClosed:
+        '''
+        TODO: Logging.info
+        '''
+        print('user disconnected')
+
             
-start_server = websockets.serve(ws_server, 'localhost', 8765)
+start_server = websockets.serve(ws_server, 'localhost', 3456)
 print('Start listening:')
 
 asyncio.get_event_loop().run_until_complete(start_server)
