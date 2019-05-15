2ea5a988d73bf725af5a844da6983b7a3e2b3234
Jim <jxia@alumni.scu.edu>
2019-05-15 12:11:14 -0700

    chipotle

--- test/client.py
+++ test/client.py
@@ -10,14 +10,17 @@ import time
 quest = {
             'header': [1111, 0, 1000, int(time.time()), 3, 0],
             'data': {
-                'query': None,
+                'query': "",
             }
         }
+
+chipotle_holder = "ws://54.218.175.199:8080/chipotle"
+local_holder = "ws://localhost:8080/chipotle"
 
 
 async def hello():
     async with websockets.connect(
-            'ws://localhost:8765') as websocket:
+            'ws://54.218.175.199:8080/chipotle') as websocket:
 
         while True:
             quest['header'][1] = int(input("app level:"))
@@ -25,8 +28,10 @@ async def hello():
             quest['data']['query'] = input("Query:")
 
             await websocket.send(json.dumps(quest))
-
+            start = time.time()
             greeting = await websocket.recv()
+            end = time.time()
+            print("round trip time: {}".format(end - start))
             print(f"< {greeting}")
 
 asyncio.get_event_loop().run_until_complete(hello())
