76b108ee6ffe0e4fb1cdb26c149f088e3e292f2b
Jim Xia <jxia@scu.edu>
2019-05-17 19:17:42 -0700

    add test file

--- test/client_newarch.py
+++ test/client_newarch.py
@@ -14,19 +14,30 @@ quest = {
             }
         }
 
+response = {
+            'header': [1111, 0, 1000, int(time.time()), 3, 0],
+            'data': {
+                'response': None,
+            }
+        }
+
 
 async def hello():
     async with websockets.connect(
             'ws://localhost:8080/chipotle') as websocket:
 
         while True:
-            quest['header'][1] = int(input("app level:"))
-            quest['header'][2] = int(input("page level:"))
             quest['data']['query'] = input("Query:")
-
             await websocket.send(json.dumps(quest))
-
             greeting = await websocket.recv()
             print(f"< {greeting}")
+
+            response['header'][2] = int(input("page level:"))
+            response['data']['response'] = input("Response:")
+            await websocket.send(json.dumps(response))
+            greeting = await websocket.recv()
+            print(f"< {greeting}")
+
+
 
 asyncio.get_event_loop().run_until_complete(hello())
